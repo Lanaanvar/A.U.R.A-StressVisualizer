a18cc5f78d5e932ce13fadb370fce420de708140
Lanaanvar <lana.anvar03@gmail.com>
2025-02-08 17:55:58 +0530

    preprocess and analysis

---
+++ backend/app/services/preprocessing.py
@@ -0,0 +1,18 @@
+import re
+import spacy
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nlp = spacy.load('en_core_web_sm')
+stopwords = set(stopwords.words('english'))
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+|www\S+|@\S+", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    doc = nlp(text)
+    lemmatized_text = [token.lemma_ for token in doc if token.text not in stopwords]
+
+    return " ".join(lemmatized_text)
